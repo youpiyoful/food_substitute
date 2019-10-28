16323a72c12fdc9f411d6f4466956dd96ebacacc
YOAN FORNARI <yoan@vitavinum.com>
2019-10-28 15:42:58 +0100

    [wip]delete sql request and create a staticMethod that say "it's ok"

--- food.py
+++ food.py
@@ -1,36 +1,15 @@
-import mysql.connector
-
-from config import config
-
-Error = mysql.connector.Error
-myDb = mysql.connector.connect(user=config.get('user'), password=config.get('password'), host=config.get('host'),
-                               port=config.get('port'), database=config.get('database'))
-
-
 class Food:
-    """Representation of food table"""
+    """Representation of food entity"""
 
-    def __init__(self):
-        self.product_name = None
-        self.generic_name = None
-        self.stores_tags = None
-        self.url = None
-        self.nutrigrade = None
-        self._id_category = None
+    def __init__(self, id_food, product_name, generic_name, stores_tags, url, nutrigrade, id_category):
+        self.id_food = id_food
+        self.product_name = product_name
+        self.generic_name = generic_name
+        self.stores_tags = stores_tags
+        self.url = url
+        self.nutrigrade = nutrigrade
+        self._id_category = id_category
 
     @staticmethod
-    def select_food_by_name(self, product_name):
-        """Select food in table food by the product name"""
-        try:
-            cursor = myDb.cursor()
-            query = f"""SELECT id_food, product_name, generic_name, stores_tags, url, nutrition_grades, id_category
-                        FROM food WHERE product_name = "{product_name}";"""
-            cursor.execute(query)
-            results = cursor.fetchall()
-            myDb.commit()
-            print(results)
-            return results
-
-        except Error as e:
-            message = f"select food encounter a mysql error : {e}"
-            print(message)
+    def its_ok():
+        return 'it\'s ok'
