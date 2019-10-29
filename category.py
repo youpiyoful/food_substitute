ed34fc51cd3738e7d3abcafdb28d9713bb150cff
YOAN FORNARI <yoan@vitavinum.com>
2019-10-29 15:15:50 +0100

    [update] add url_category in category entity

--- category.py
+++ category.py
@@ -1,6 +1,7 @@
 class Category:
     """Representation of category table"""
 
-    def __init__(self, id_category, category_name):
+    def __init__(self, id_category, category_name, url_category):
         self.id_category = id_category
         self.category_name = category_name
+        self.url_category = url_category
