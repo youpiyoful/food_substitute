ebd1c48ea15e14bdb759be2e3c4d6d1bd8891ef5
YOAN FORNARI <yoan@vitavinum.com>
2019-10-29 15:09:32 +0100

    [wip] add retrieve_food_with_url_category, and add a constructor for variable of url in retrieve_all_category_name_from_open_food_facts_api

--- open_food_facts.py
+++ open_food_facts.py
@@ -5,40 +5,70 @@ from config import list_of_keywords_interesting
 
 
 class OpenFoodFact:
+    def __init__(self, url_categories):
+        self.url_categories = url_categories
 
-    @staticmethod
-    def retrieve_all_category_name_from_open_food_facts_api():
+    def retrieve_all_category_name_from_open_food_facts_api(self):
         """
         This function return a list of category's name of food from open food fact api
-        :return list of name of category food:
+        :return list_of_new_category of food:
         """
-        response = requests.get('https://fr.openfoodfacts.org/v0/categories.json')
+        response = requests.get(self.url_categories)
         list_of_category = response.json().get('tags')
-        list_of_category_names = []
+        list_of_new_category = []
 
         for category in list_of_category:
-            name_category = category.get('name')
-            list_of_category_names.append(name_category)
+            new_category = {
+                'category_name': category.get('name'),
+                'url_category': category.get('url')
+            }
+            list_of_new_category.append(new_category)
 
-        return list_of_category_names
+        return list_of_new_category
 
     @staticmethod
-    def filter_category_by_interest(list_of_category_names, list_of_keywords):  # idea : do a forbidden list !
+    def retrieve_food_with_url_category(url_category, id_category):
         """
-        :param list_of_category_names:
+        :param url_category:
+        :param id_category:
+
+        :return:
+        """
+        response = requests.get(url_category + '.json')
+        list_of_food = response.json().get('products')
+        list_of_food_by_category = []
+
+        for food in list_of_food:
+            new_food = {
+                'product_name': food.get('product_name'),
+                'generic_name': food.get('generic_name'),
+                'stores_tags': food.get('stores_tags'),
+                'url': food.get('url'),
+                'nutrition_grades': food.get('nutriscore_score'),
+                'id_category': id_category
+            }
+            list_of_food_by_category.append(new_food)
+
+        return list_of_food_by_category
+
+    @staticmethod
+    def filter_category_by_interest(list_of_new_category, list_of_keywords):  # idea : do a forbidden list !
+        """
+        :param list_of_new_category:
         :param list_of_keywords:
         :return list_of_interesting_category:
         """
         list_of_interesting_category = []
 
-        for category_name in list_of_category_names:
+        for new_category in list_of_new_category:
+            category_name = new_category.get('category_name')
 
             for keywords in list_of_keywords:
 
                 if keywords in category_name:
 
                     if 'viande' not in category_name or 'lait' not in category_name:
-                        list_of_interesting_category.append(category_name)
+                        list_of_interesting_category.append(new_category)
                         break
 
         return list_of_interesting_category
