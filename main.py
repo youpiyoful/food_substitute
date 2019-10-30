3c2bf60f6ca37f907e0a6867e30a0c46e9ea1101
YOAN FORNARI <yoan@vitavinum.com>
2019-10-30 13:56:51 +0100

    [wip][refactoring] refactor data connection, variable is now in config.py wip on main function able to display best food now

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import config
 import manager
 import open_food_facts
 
-open_food_facts_object = open_food_facts.OpenFoodFact(
-    url_category='https://fr.openfoodfacts.org/categories.json')  # instance of OpenFoodFact
+open_food_facts_object = open_food_facts.OpenFoodFact(config.url_categories,
+                                                      config.number_min_food_by_category)  # instance of OpenFoodFact
 manage_categories = manager.ManageCategories()  # instance of ManageCategories
 manage_food = manager.ManageFood()  # instance of ManageFood
 
@@ -42,24 +42,45 @@ def retrieve_food_from_open_food_facts_and_insert_in_our_db():
 
 
 # retrieve_food_from_open_food_facts_and_insert_in_our_db()
-food = manage_food.get_food_by_name('Chips Allégées en Petit Sachet')
-print(food.product_name)
+
+
+# food = manage_food.get_food_by_name('Chips Allégées en Petit Sachet')
+# print(food.product_name)
 
 
 def main():
-    # food_by_name = manage_food.get_food_by_name(
-    #     'test')  # use method of ManageFood than instantiate Food with data requested in db
+    all_categories = manage_categories.get_category_where_they_contain_15_food_min()  # use method
 
-    all_categories = manage_categories.get_category()  # use method
-    print(all_categories[0])
+    for category in all_categories:  # run all category object in all_categories list
+        print(category.id_category, category.category_name)  # display category attribute
 
-    # for category in all_categories:  # run all category object in all_categories list
-    #     print(category.id_category, category.category_name)  # display category attribute
-    #
-    # category_choice = int(input('Enter the num of your choice'))
-    #
-    # # print(food_by_name.product_name)  # display product name of food object
-    # # print(food_by_name.__dict__)
+    category_choice = int(input('Enter the num of your choice : '))
+    list_of_food_by_id_category = manage_food.get_food_by_id_category(
+        category_choice)  # use method of ManageFood than instantiate Food with data requested in db
+
+    for food_by_id_category in list_of_food_by_id_category:
+        print(food_by_id_category.id_food, food_by_id_category.product_name)
+
+    food_choice = int(input('Enter the num of your choice : '))
+
+    for food_by_id_category in list_of_food_by_id_category:
+
+        if food_choice == food_by_id_category.id_food:
+            best_food = food_by_id_category.get_food_best_than_me(list_of_food_by_id_category)
+            print('')
+            print("              BEST FOOD                ")
+            print('')
+            print('id_food          : ', best_food.id_food)
+            print('product_name     : ', best_food.product_name)
+            print('generic name     : ', best_food.generic_name)
+            print('stores_tags      : ', best_food.stores_tags)
+            print('link to product  : ', best_food.url)
+            print('nutriscore-score : ', best_food.nutrigrade)
+            print('id_category      : ', best_food.id_category)
+
+
+    # print(list_of_food_by_id_category.product_name)  # display product name of food object
+    # print(list_of_food_by_id_category.__dict__)
     # print("""
     #             ########   #######   #######   ##########
     #            ##         ##   ##   ##   ##     ##    ##
