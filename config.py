51adfb4407bde12f5a5c5411f2c1f2d2291dacd8
YOAN FORNARI <yoan@vitavinum.com>
2019-10-30 11:11:59 +0100

    [delete]delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-list_of_keywords_interesting = ['Céréales', 'végétaux', 'Fruits', 'végétale', 'légumes']
-
-config = {
-    'user': 'yoan',
-    'password': 'man1306',
-    'host': 'je ne sais pas',
-    'database': 'FOOD'
-}
