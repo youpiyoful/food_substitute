1f37c8ee22c596938a3441125e73da7603aaecd7
YOAN FORNARI <yoan@vitavinum.com>
2019-10-30 13:49:49 +0100

    [update] retrieve_food_with_url_category have now a limit minimum of food by category and use the system of page of open food facts api

--- open_food_facts.py
+++ open_food_facts.py
@@ -1,12 +1,11 @@
 """This file processes data recovery and inserting into tables of our data base"""
 import requests
-import config
-from config import list_of_keywords_interesting
 
 
 class OpenFoodFact:
-    def __init__(self, url_categories):
+    def __init__(self, url_categories='https://fr.openfoodfacts.org/categories.json', number_min_food_by_category=10):
         self.url_categories = url_categories
+        self.number_min_food_by_category = number_min_food_by_category
 
     def retrieve_all_category_name_from_open_food_facts_api(self):
         """
@@ -26,28 +25,55 @@ class OpenFoodFact:
 
         return list_of_new_category
 
-    @staticmethod
-    def retrieve_food_with_url_category(url_category, id_category):
+    def retrieve_food_with_url_category(self, url_category, id_category):
         """
         :param url_category:
         :param id_category:
 
         :return:
         """
-        response = requests.get(url_category + '.json')
-        list_of_food = response.json().get('products')
         list_of_food_by_category = []
+        num_page = 1
+        number_of_product = 0
 
-        for food in list_of_food:
-            new_food = {
-                'product_name': food.get('product_name'),
-                'generic_name': food.get('generic_name'),
-                'stores_tags': food.get('stores_tags'),
-                'url': food.get('url'),
-                'nutrition_grades': food.get('nutriscore_score'),
-                'id_category': id_category
-            }
-            list_of_food_by_category.append(new_food)
+        # maximum_time = 10
+        # tick = time.time()
+        # todo verify number of product by page
+
+        while len(list_of_food_by_category) <= self.number_min_food_by_category:
+            response = requests.get(url_category + '/' + str(num_page) + '.json')
+            list_of_food = response.json().get('products')
+            print('list of food : ', list_of_food)
+            # number_of_product += len(list_of_food)
+            # print('number of product : ', number_of_product)
+            num_page += 1
+            print('number of page : ', num_page)
+            # print(tick)
+            # passed_time = time.time() - tick
+            # print(passed_time)
+            # if passed_time > maximum_time:
+            #     break
+
+            if len(list_of_food) == 0:
+                break
+
+            for food in list_of_food:
+
+                if food.get('product_name') and food.get('generic_name') and food.get('url') and food.get(
+                        'nutriscore_score') and id_category:
+                    new_food = {
+                        'product_name': food.get('product_name'),
+                        'generic_name': food.get('generic_name'),
+                        'stores_tags': food.get('stores_tags'),
+                        'url': food.get('url'),
+                        'nutrition_grades': food.get('nutriscore_score'),
+                        'id_category': id_category
+                    }
+                    list_of_food_by_category.append(new_food)
+            print(len(list_of_food_by_category))
+
+            if len(list_of_food) < 20:
+                break
 
         return list_of_food_by_category
 
@@ -72,7 +98,6 @@ class OpenFoodFact:
                         break
 
         return list_of_interesting_category
-
 
 # response = OpenFoodFact()
 # response.retrieve_all_category_name_from_open_food_facts_api()
