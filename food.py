c7b609f0075c0181cd2e7199a85418e95f8f590c
YOAN FORNARI <yoan@vitavinum.com>
2019-10-30 13:54:37 +0100

    [add] add get_food_best_than able to find best food in a category

--- food.py
+++ food.py
@@ -6,16 +6,49 @@ class Food:
 
     def __init__(self, id_food, product_name,
                  generic_name, stores_tags, url, nutrigrade, id_category):
-        """Initialize"""
+        """
+        Initialize
+        :param id_food:
+        :param product_name:
+        :param generic_name:
+        :param stores_tags:
+        :param url:
+        :param nutrigrade:
+        :param id_category:
+        """
         self.id_food = id_food
         self.product_name = product_name
         self.generic_name = generic_name
         self.stores_tags = stores_tags
         self.url = url
         self.nutrigrade = nutrigrade
-        self._id_category = id_category
+        self.id_category = id_category
 
     @staticmethod
     def its_ok():
         """Test than object work"""
         return 'it\'s ok'
+
+    def get_food_best_than_me(self, list_of_food):
+        """
+        calcul the food than have the best nutriscore
+        :param list_of_food:
+        :return:
+        """
+        list_of_id_of_best_food = []
+
+        for food_object in list_of_food:
+
+            if food_object.nutrigrade < self.nutrigrade:
+                list_of_id_of_best_food.append(food_object.id_food)
+
+        if list_of_id_of_best_food:
+            id_of_best_food = min(list_of_id_of_best_food)
+
+            for food_object in list_of_food:
+
+                if id_of_best_food == food_object.id_food:
+                    return food_object
+
+        else:
+            return self
