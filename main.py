4140a0d118933530d52e323a888efc5368ec7175
youpiyoful <yoanfornari@gmail.com>
2019-11-04 15:24:11 +0100

    [refactoring] add commentary + design line for shell

--- main.py
+++ main.py
@@ -12,62 +12,88 @@ def main():
     """
     program_on = True
 
-    while program_on is True:
-        good_answer = False
+    while program_on is True:  # main loop, start the program
+        good_answer = False  # variable of control
         best_food = ""  # referenced before assignment to best_food variable
         start_choice = ""  # referenced before assignment to best_food variable
         leave_or_no = ""  # referenced before assignment to best_food variable
-        category_choice = ""
-        food_choice = ""
+        category_choice = ""  # referenced before assignment to best_food variable
+        food_choice = ""  # referenced before assignment to best_food variable
         list_of_id_category = []
         list_of_id_food = []
-
-        while good_answer is False:  # start the program
+        print("""
+                        ########  ##    ##  ########  ########  ########  ########  ########  ##    ##  ########  ########
+                       ##        ##    ##  ##    ##  ##           ##        ##        ##     ##    ##     ##     ##
+                      ########  ##    ##  ##  ####  ########     ##        ##        ##     ##    ##     ##     ########
+                           ##  ##    ##  ##    ##        ##     ##        ##        ##     ##    ##     ##     ##
+                    ########  ########  ########   ########    ##     ########     ##     ########     ##     ########
+        """)
+        print("""
+                                                    ########
+        """)
+        print("""
+                    ########   #######   #######   ##########
+                   ##         ##   ##   ##   ##     ##    ##
+                  ######     ##   ##   ##   ##     ##    ##
+                 ##         ##   ##   ##   ##     ##    ##
+                ##         #######   #######   ##########
+        """)
+        # menu
+        while good_answer is False:  # while good answer is false the loop continue
             print('')
             start_choice = input(
                 'For consult your substitution record push 1 | For launch the program of '
                 'substitution '
-                'push 2 : ')
+                'push 2 : ')  # offers 2 choices at user
             print('')
 
-            if start_choice == '1' or start_choice == '2':
+            if start_choice == '1' or start_choice == '2':  # if user do an offer choice,
+                # good answer become true and the loop parent loop is done
                 good_answer = True
 
-        good_answer = False
-        if start_choice == '2':
-            all_categories = MANAGE_CATEGORIES.get_category_where_they_contain_15_food_min()  # use
-            # method
+        good_answer = False  # reset the variable of control
+
+        if start_choice == '2':  # listen user's choice
+            # recovers all categories containing more than 15 foods
+            all_categories = MANAGE_CATEGORIES.get_category_where_they_contain_15_food_min()
 
             for category in all_categories:  # run all category object in all_categories list
                 print(category.id_category, category.category_name)  # display category attribute
+                # creates a category id list and converts them to a string
                 list_of_id_category.append(str(category.id_category))
 
-            while category_choice not in list_of_id_category:
+            while category_choice not in list_of_id_category:  # choice of the category
                 print('')
                 category_choice = input('Enter the num of your choice : ')
                 print('')
 
-            # use method of ManageFood than instantiate Food with data requested in db
+            # use method of ManageFood than instantiate Food object with data requested in db
             list_of_food_by_id_category = MANAGE_FOOD.get_food_by_id_category(
                 category_choice)
 
+            #  display all the foods in the category chosen by the user
             for food_by_id_category in list_of_food_by_id_category:
                 print(food_by_id_category.id_food, food_by_id_category.product_name)
+                # save in a list the id foods and converts them into a chain
                 list_of_id_food.append(str(food_by_id_category.id_food))
 
+            # food choice that the user wants to substitute for a better product if there is one
             while food_choice not in list_of_id_food:
                 print('')
-                # need to don t convert choice to int for handle bad string not only bad int
+                # don't convert the user's choice to an integer, even if it's necessary later,
+                # to be able to manage the erroneous choices
                 food_choice = input('Enter the num of your choice : ')
                 print('')
 
+            # displays the substitute food of the user's chosen food only if it offers a better
+            # nutrient
             for food_by_id_category in list_of_food_by_id_category:
 
                 if food_choice == str(food_by_id_category.id_food):
                     best_food = food_by_id_category.get_food_best_than_me(
                         list_of_food_by_id_category)
                     print('')
-                    print('              BEST FOOD                ')
+                    print('              BEST FOOD IF IT EXISTS              ')
                     print('')
                     print('id_food          : ', best_food.id_food)
                     print('product_name     : ', best_food.product_name)
@@ -78,6 +104,8 @@ def main():
                     print('id_category      : ', best_food.id_category)
                     print('')
 
+            #  4th step of the program, which proposes to the user to save the substitution he
+            #  has just made
             while good_answer is False:
                 response_to_record = input('Want you record substitute food ? y/n : ')
 
@@ -91,11 +119,13 @@ def main():
                     print('don\'t record subsitute')
                     good_answer = True
 
+        # part that displays the substitutions already made
         else:
 
+            # uses the show_substitution method of the manage_food object
             substitutes_elements = MANAGE_FOOD.show_substitution()
 
-            for substitute_el in substitutes_elements:
+            for substitute_el in substitutes_elements:  # formats the display
                 print('---------------------------------------------------------------------------')
                 print('category name            : ', substitute_el[0])
                 print('product name substituted : ', substitute_el[1])
@@ -106,33 +136,28 @@ def main():
                 print('link                     : ', substitute_el[6])
                 print('---------------------------------------------------------------------------')
 
-        good_answer = False
+        good_answer = False  # reset the control variable
 
+        # last step that proposes to leave the program or return to the menu in the main loop
         while good_answer is False:
-            leave_or_no = input('Have you finished ? (y/n): ')
+            leave_or_no = input('Have you finished ? (y/n): ')  # listen user's choice
 
-            if leave_or_no == 'y' or leave_or_no == 'n':
+            if leave_or_no == 'y' or leave_or_no == 'n':  # control than answer is good
                 good_answer = True
 
-        if leave_or_no == 'y':
+        if leave_or_no == 'y':  # verify if the user want leave the program
+            # sets the program_on variable to false, which results in quitting the program
+            # because the initial condition of the first loop is no longer satisfied
             program_on = False
 
-        else:
+        else:  # if answer is no the program comeback to the menu
             print('')
-            print('                    COMEBACK TO MENU          ')
+            print('                    COMEBACK TO THE MENU          ')
             print('')
 
     print('')
     print('                       END                   ')
     print('')
-
-    # print("""
-    #             ########   #######   #######   ##########
-    #            ##         ##   ##   ##   ##     ##    ##
-    #           ######     ##   ##   ##   ##     ##    ##
-    #          ##         ##   ##   ##   ##     ##    ##
-    #         ##         #######   #######   ##########
-    # """)
 
 
 main()
